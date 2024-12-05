771ac63a8bca7e27813bcbc6bfbd9ea75d28cd3d
b-nn <bananann64@gmail.com>
2024-12-05 20:22:12 +0200

    apparently forgot to commit main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 // mod day1;
 // mod day2;
-mod day3;
+// mod day3;
+// mod day4;
+mod day5;
 const REPEAT: i32 = 20;
 
 fn main() {
-    let x = day3::run();
-    // println!("Puzzle 1: {} \nPuzzle 2: {}", x, y);
-    println!("{}", x);
+    let (x, y) = day5::run();
+    println!("Part 1: {} \nPart 2: {}", x, y);
 }
