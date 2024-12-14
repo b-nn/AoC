4403e93987f8e5fe81a1b5f25359770954bcb303
b-nn <bananann64@gmail.com>
2024-12-14 13:34:16 +0200

    made day14.rs

---
+++ src/day14.rs
@@ -0,0 +1,21 @@
+use crate::REPEAT;
+use std::fs;
+use std::time::Instant;
+
+pub fn run() -> ((i64, i64), (Vec<u128>, Vec<u128>, Vec<u128>, Vec<u128>)) {
+    let mut read: Vec<u128> = vec![];
+    let mut cleanup: Vec<u128> = vec![];
+    let mut part1t: Vec<u128> = vec![];
+    let mut part2t: Vec<u128> = vec![];
+    let mut part1: i64 = 0;
+    let mut part2: i64 = 0;
+
+    for _i in 0..REPEAT {
+        let now = Instant::now();
+        let content =
+            fs::read_to_string("day14.txt").expect("THERE'S NO INPUT WHAT THE FUCKKKKKKKK");
+        read.push(now.elapsed().as_nanos());
+    }
+
+    ((part1, part2), (read, cleanup, part1t, part2t))
+}
