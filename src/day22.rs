999643d783f89d39943ec681ede9892f7bef751a
b-nn <bananann64@gmail.com>
2024-12-23 17:22:34 +0200

    yeah

--- src/day22.rs
+++ src/day22.rs
@@ -5,10 +5,12 @@ use std::collections::{hash_set, HashMap, HashSet};
 use std::fs;
 use std::time::Instant;
 
-fn cycle(mut input: &mut i64) {
-    *input = ((*input << 6) ^ *input) & 0xFFFFFF;
-    *input = ((*input >> 5) ^ *input) & 0xFFFFFF;
-    *input = ((*input << 11) ^ *input) & 0xFFFFFF;
+fn cycle(mut input: &mut i32) {
+    let mut t = *input as i64;
+    t = ((t << 6) ^ t) & 0xFFFFFF;
+    t = ((t >> 5) ^ t) & 0xFFFFFF;
+    t = ((t << 11) ^ t) & 0xFFFFFF;
+    *input = t as i32;
 }
 
 pub fn run() -> ((i64, i64), (Vec<u128>, Vec<u128>, Vec<u128>, Vec<u128>)) {
@@ -17,7 +19,7 @@ pub fn run() -> ((i64, i64), (Vec<u128>, Vec<u128>, Vec<u128>, Vec<u128>)) {
     let mut part1t: Vec<u128> = vec![];
     let mut part2t: Vec<u128> = vec![];
     let mut part1: i64 = 0;
-    let mut part2: i64 = 0;
+    let mut part2: i32 = 0;
 
     for _i in 0..REPEAT {
         let now = Instant::now();
@@ -28,35 +30,28 @@ pub fn run() -> ((i64, i64), (Vec<u128>, Vec<u128>, Vec<u128>, Vec<u128>)) {
         let now = Instant::now();
         let mut pseudo = content
             .lines()
-            .map(|x| x.parse::<i64>().expect("unable to parse"))
+            .map(|x| x.parse::<i32>().expect("unable to parse"))
             .collect::<Vec<_>>();
-        let mut hash_total = HashMap::new();
+        let mut hash_total: HashMap<i32, i32> = HashMap::new();
         cleanup.push(now.elapsed().as_nanos());
 
         let now = Instant::now();
-        let mut hash = HashSet::new();
-        let mut v1 = 0;
-        let mut v2 = 0;
-        let mut v3 = 0;
-        let mut v4 = 0;
+        let mut hash: HashSet<i32> = HashSet::new();
+        let mut key = 0;
         for mut i in pseudo {
             hash.drain();
             for j in 0..2000 {
                 let last = i;
                 cycle(&mut i);
-                v1 = v2;
-                v2 = v3;
-                v3 = v4;
-                v4 = i % 10 - last % 10 & 0b11111;
+                key = ((key << 5) ^ (i % 10 - last % 10)) & 0b11111111111111111111;
                 if j >= 3 {
-                    let key = (v1 | v2 << 5 | v3 << 10 | v4 << 15);
                     if hash.get(&key).is_none() {
                         *hash_total.entry(key).or_insert(0) += i % 10;
                         hash.insert(key);
                     }
                 }
             }
-            part1 += i;
+            part1 += i as i64;
         }
         for i in hash_total {
             if part2 < i.1 {
@@ -67,5 +62,8 @@ pub fn run() -> ((i64, i64), (Vec<u128>, Vec<u128>, Vec<u128>, Vec<u128>)) {
         part2t.push(now.elapsed().as_nanos());
     }
 
-    ((part1, part2), (read, cleanup, part1t, part2t))
+    (
+        (part1 as i64, part2 as i64),
+        (read, cleanup, part1t, part2t),
+    )
 }
